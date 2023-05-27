a4b8d290d78080657fb4100034d92650b3682e88
silchenko-arsen <95136538+silchenko-arsen@users.noreply.github.com>
2023-05-27 17:12:05 +0000

    added go-up-button

---
+++ js/go-up-button.js
@@ -0,0 +1,19 @@
+const goUpBtn = document.querySelector(".go-up-button");
+
+goUpBtn.addEventListener("click", goUp);
+window.addEventListener("scroll", trackScroll);
+
+function trackScroll() {
+    const offset = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+    if (offset > coords) {
+        goUpBtn.classList.add("go-up-button--show");
+    } else {
+        goUpBtn.classList.remove("go-up-button--show");
+    }
+}
+
+function goUp() {
+    document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("partialsLoaded", () => {
+    import('./go-up-button.js');
+
+  });
