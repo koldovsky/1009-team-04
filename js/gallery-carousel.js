e7f8f2b659c2d24425a8d2be5f1972536f3cbaa0
matsy-p <131802759+matsy-p@users.noreply.github.com>
2023-06-02 17:24:22 +0000

    rework carousel

--- js/gallery-carousel.js
+++ js/gallery-carousel.js
@@ -6,45 +6,58 @@ const carouselItems = carouselItem.querySelectorAll('.carousel__item');
 const dotsContainer = carousel.querySelector('.carousel__dots');
 
 let currentIndex = 0;
+let pageSize = calculatePageSize();
+
+function calculatePageSize() {
+    if (window.innerWidth >= 1024) {
+        return 3;
+    } else if (window.innerWidth >= 768) {
+        return 2;
+    } else {
+        return 1;
+    }
+};
 
 function createDots() {
-    for (let i = 0; i < carouselItems.length; i++) {
+    const numItems = carouselItems.length;
+    const numDots = Math.ceil(numItems / pageSize);
+  
+    for (let i = 0; i < numDots; i++) {
         const dot = document.createElement('div');
         dot.classList.add('carousel__dot');
         dot.addEventListener('click', () => {
-            currentIndex = i;
+            currentIndex = i * pageSize;
             updateCarousel();
         });
         dotsContainer.appendChild(dot);
     }
-}
+};
 
 function updateDots() {
     const dots = dotsContainer.querySelectorAll('.carousel__dot');
     dots.forEach((dot, index) => {
-        dot.classList.toggle('active', index === currentIndex);
+        dot.classList.toggle('active', currentIndex >= index * pageSize && currentIndex < (index + 1) * pageSize);
     });
-}
+};
 
 function updateCarousel() {
-    carouselItem.style.transform = `translateX(-${currentIndex * 100}%)`;
+    carouselItem.style.transform = `translateX(-${currentIndex * (100 / pageSize)}%)`;
     updateDots();
-}
+};
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    currentIndex = (currentIndex - pageSize + carouselItems.length) % carouselItems.length;
     updateCarousel();
 });
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % carouselItems.length;
+    currentIndex = (currentIndex + pageSize) % carouselItems.length;
     updateCarousel();
 });
 
 carouselItem.addEventListener('transitionend', () => {
-    if (currentIndex === carouselItems.length - 1) {
+    if (currentIndex === carouselItems.length - pageSize) {
         carouselItem.style.transition = 'none';
-        
         updateCarousel();
     } else if (currentIndex === 0) {
         carouselItem.style.transition = 'none';
@@ -52,9 +65,15 @@ carouselItem.addEventListener('transitionend', () => {
     }
 });
 
-window.addEventListener('resize', updateCarousel);
+window.addEventListener('resize', () => {
+    const newPageSize = calculatePageSize();
+    if (pageSize !== newPageSize) {
+        pageSize = newPageSize;
+        dotsContainer.innerHTML = '';
+        createDots();
+        updateCarousel();
+    }
+});
 
 createDots();
 updateCarousel();
-
-
