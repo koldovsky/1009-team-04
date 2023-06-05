35834ff02d3657346cac13fad450bd3d8418df95
silchenko-arsen <95136538+silchenko-arsen@users.noreply.github.com>
2023-06-05 18:27:36 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,7 @@
 document.addEventListener("partialsLoaded", () => {
-<<<<<<< HEAD
-    import('./go-up-button.js');
-    import('./carousel.js');
-    import('./menu-history.js');
-  });
-=======
   import('./go-up-button.js');
   import('./testimonials-carousel.js');
   import('./gallery-carousel.js');
   import('./age-restriction.js');
 });
->>>>>>> 3bc177a94780cbfc0762dde3b1072fc1e6d26cdc
+
