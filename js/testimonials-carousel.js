003d025b8f132407953fc71a30ca94bee3965be3
YBortnyk <ufrait@gmail.com>
2023-06-15 15:18:53 +0300

    fixed some code in testimonials-carousel.js

--- js/testimonials-carousel.js
+++ js/testimonials-carousel.js
@@ -1,43 +1,49 @@
 const carousel = document.querySelector('.testimonials__carousel');
-const carouselInner = carousel.querySelector('.carousel__feedback-list');
+const carouselItem = carousel.querySelector('.carousel__feedback-list');
 const prevButton = carousel.querySelector('.button--left');
 const nextButton = carousel.querySelector('.button--right');
-const carouselCards = carouselInner.querySelectorAll('.carousel__feedback-card');
+const carouselItems = carouselInner.querySelectorAll('.carousel__feedback-card');
 
 let currentIndex = 0;
-let cardsInARow;
-let cardSize;
+let pageSize = calculatePageSize();
 
-setCardsSettings();
-window.addEventListener("resize", setCardsSettings, true);
-
-
-function setCardsSettings() {
-    if (window.innerWidth >= 1024) cardsInARow = 4;
-    else if (window.innerWidth > 768) cardsInARow = 2;
-    else cardsInARow = 1;
-
-    cardSize = 100 / cardsInARow;
-    currentIndex = 0;
-	updateCarousel();
+function calculatePageSize() {
+    if (window.innerWidth >= 1024) {
+        return 4;
+    } else if (window.innerWidth >= 992) {
+        return 3;
+    } else if (window.innerWidth >= 768) {
+        return 2;
+    } else {
+        return 1;
+    }
 };
-
 
 function updateCarousel() {
-    carouselInner.style.transform = `translateX(-${currentIndex * cardSize}%)`;
-};
+    const totalItems = carouselItems.length;
+    const remainingItems = totalItems - currentIndex;
+    const visibleItems = Math.min(pageSize, remainingItems);
+
+carouselItem.style.transform = `translateX(-${currentIndex * (100 / pageSize)}%)`;
+
+if (remainingItems < pageSize + 1) {
+    const emptySpace = pageSize + 1 - remainingItems;
+    for (let i = 0; i < emptySpace; i++) {
+        const cloneIndex = (currentIndex + i) % totalItems;
+        const cloneItem = carouselItems[cloneIndex].cloneNode(true);
+        cloneItem.classList.add('carousel__feedback-card--clone');
+        carouselItem.appendChild(cloneItem);
+    }
+}
+}
 
 prevButton.addEventListener('click', () => {
-    if (currentIndex === 0) currentIndex = carouselCards.length - cardsInARow;
-    else currentIndex--;
-    
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
     updateCarousel();
 });
 
 nextButton.addEventListener('click', () => {
-    if (currentIndex + cardSize === carouselCards.length) currentIndex = 0;
-    else currentIndex++;
-
+    currentIndex = (currentIndex + 1) % carouselItems.length;
     updateCarousel();
 });
 
